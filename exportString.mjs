d976a07fb9ec94bc116c07f64674be5f51ea7ca3
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-21 09:38:48 +0000

    added files and stirng

---
+++ exportString.mjs
@@ -0,0 +1,4 @@
+// Import and Export a String using ES6 Module Syntax
+
+let stringValue = "Be Happy and Safe";
+export default stringValue;
---
+++ importString.mjs
@@ -0,0 +1,4 @@
+// Import and Export a String using ES6 Module Syntax
+
+import stringValue from "./exportString.mjs";
+console.log(stringValue);
